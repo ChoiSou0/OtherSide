762ef26bde818a32cd84496e68cc05f7cd97e09f
ChoiSou0 <95004780+ChoiSou0@users.noreply.github.com>
2022-10-12 03:35:19 +0900

    스테이지3은 안개랑 물어보면 끝일듯

---
+++ Assets/Shader_Choi/IceWalk.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceWalk : MonoBehaviour
+{
+    void IceWalking()
+    {
+        //float moveX = Input.GetAxis("Horizontal") * Speed;
+        //float moveZ = Input.GetAxis("Vertical") * Speed;
+        //rb.velocity = new Vector3(moveX, rb.velocity, moveZ);
+    }
+}
