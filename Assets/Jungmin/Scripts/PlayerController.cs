d41f097e529131c9e09a788531e81b77ee2769db
JeongminKim <kjmgi0671@naver.com>
2022-10-25 00:28:59 +0900

    2스테이지 수정
    안개 / 빛 추가하면 될듯

--- Assets/Jungmin/Scripts/PlayerController.cs
+++ Assets/Jungmin/Scripts/PlayerController.cs
@@ -43,7 +43,7 @@ public class PlayerController : MonoBehaviour
         }
 
         //중력 가속도
-        gravityVelocity += (Time.deltaTime * Physics.gravity.y) * 0.3f;
+        gravityVelocity = Time.deltaTime * Physics.gravity.y;
         if (isGrounded) gravityVelocity = 0f;
 
         rb.velocity = movePos + Vector3.up * gravityVelocity;
