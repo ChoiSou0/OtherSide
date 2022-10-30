4c91147ffacd678cc2a20b2c011504258ddf5388
ChoiSou0 <95004780+ChoiSou0@users.noreply.github.com>
2022-10-31 02:29:37 +0900

    나도 잘 모르겄다 이게 맞는지

---
+++ Assets/Shader_Choi/Scripts/Cam_Ctrl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace Cam_Control
+{
+    public class Cam_Ctrl : MonoBehaviour
+    {
+        public static void Enlargement(Transform EnlargementPosition, GameObject Cam)
+        {
+            Transform NowPos = Cam.transform;
+            // 아래 코드는 확대 관련
+        }
+
+
+        
+    }
+}
