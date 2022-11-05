dd43fa69484f514d52830ff9c7c38ea08392b5c3
ChoiSou0 <95004780+ChoiSou0@users.noreply.github.com>
2022-11-05 19:02:41 +0900

    캠 흔들기 수정중

--- Assets/Shader_Choi/Scripts/Cam_Ctrl.cs
+++ Assets/Shader_Choi/Scripts/Cam_Ctrl.cs
@@ -7,8 +7,9 @@ namespace Cam_Control
 {
     public class Cam_Ctrl : MonoBehaviour
     {
-    static Transform NowPos;
+        static Transform NowPos;
 
+        // 카메라 확대
         public static IEnumerator Enlargement(GameObject Cam, float Distance, float Time)
         {
             NowPos = Cam.transform;
@@ -19,6 +20,7 @@ namespace Cam_Control
             yield break;
         }
 
+        // 카메라 축소
         public static IEnumerator Reduction(GameObject Cam, float Distance, float Time)
         {
             NowPos = Cam.transform;
@@ -29,6 +31,27 @@ namespace Cam_Control
             yield break;    
         }
 
-        
+        public void Shake(Vector3 cameraPos, float Duration, float ShakeRange)
+        {
+            cameraPos = Camera.main.transform.position;
+            InvokeRepeating(StartShake(ShakeRange), 0f, 0.005f);
+            Invoke("StopShake", ShakeRange);
+        }
+
+        private void StartShake(float ShakeRange)
+        {
+            float cameraPosX = Random.value * ShakeRange * 2 - ShakeRange;
+            float cameraPosY = Random.value * ShakeRange * 2 - ShakeRange;
+            Vector3 cameraPos = Camera.main.transform.position;
+            cameraPos.x += cameraPosX;
+            cameraPos.y += cameraPosY;
+            Camera.main.transform.position = cameraPos;
+        }
+
+        private void StopShake(Vector3 cameraPos)
+        {
+            CancelInvoke("StartShake");
+            Camera.main.transform.position = cameraPos;
+        }
     }
 }
