fa7827f6758159f4221f4050b1570df8df508693
JeongminKim <kjmgi0671@naver.com>
2022-11-08 01:28:35 +0900

    졸려서 못버틴다

--- Assets/Jungmin/Scripts/TestScripts/PlayerScript/Player.cs
+++ Assets/Jungmin/Scripts/TestScripts/PlayerScript/Player.cs
@@ -1,35 +1,50 @@
 using Jungmin;
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.IO.IsolatedStorage;
+using System.Linq;
 using UnityEngine;
+
+public enum PlayerType
+{
+    Basic,
+    Follow,
+}
 
 public class Player : Controller
 {
-    public System.Action<Transform> OtherPlayerFollowMe;
+    public System.Action<Transform> OtherPlayerFollowMe = null;
+    public System.Func<int, bool> MovePlayerDecision = null;
 
+    public PlayerType playerType;
     [SerializeField] private Player OtherPlayer = null;
-    public bool isFollow;
-    public bool isActive;
 
     protected override void Awake()
     {
         base.Awake();
-        if (OtherPlayer != null && isFollow)
-        {
-            OtherPlayer.OtherPlayerFollowMe += FollowOther;
-        }
+        CheckOtherPlayer();
     }
 
     protected override void Update()
     {
         base.Update();
     }
+    private void CheckOtherPlayer()
+    {
+        if (OtherPlayer == null) return;
+
+        if (playerType == PlayerType.Follow)
+        {
+            OtherPlayer.OtherPlayerFollowMe += this.FollowOther;
+        }
+        else if (playerType == PlayerType.Basic && OtherPlayer.playerType == PlayerType.Basic)
+        {
+            MovePlayerDecision += this.ShortPathThenOther;
+        }
+    }
 
     protected override void TouchScreen()
     {
-        if (!isActive) return;
+        if (playerType == PlayerType.Follow) return;
         base.TouchScreen();
     }
 
@@ -39,7 +54,25 @@ public class Player : Controller
         OtherPlayerFollowMe?.Invoke(pathList[pathList.Count - 2]);
     }
 
-    //다른 플레이어 따라가기
+    protected override void FollowPath()
+    {
+        if (MovePlayerDecision != null)
+        {
+            if (!MovePlayerDecision(OtherPlayer.walkPathQueue.Count))
+            {
+                StopWalking();
+                return;
+            }
+        }
+        base.FollowPath();
+    }
+
+    private bool ShortPathThenOther(int otherPathCount)
+    {
+        print(otherPathCount);
+        if (otherPathCount > this.walkPathQueue.Count || otherPathCount == 0) return true;
+        else return false;
+    }
     private void FollowOther(Transform target)
     {
         StopWalking();
