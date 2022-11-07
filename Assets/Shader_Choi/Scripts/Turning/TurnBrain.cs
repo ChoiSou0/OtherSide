343b2bc7e4ef48c707d17ffe79eaa2e5074cde1a
Jeongmin Kim <kjmgi0671@naver.com>
2022-11-07 19:39:46 +0900

    revert

---
+++ Assets/Shader_Choi/Scripts/Turning/TurnBrain.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnBrain : MonoBehaviour
+{
+    public List<Turn_Info> TurnObject;
+    private bool isTurning;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Turning();
+    }
+
+    private void Turning()
+    {
+        if (Input.GetMouseButtonDown(0) && !isTurning)
+        {
+            Debug.Log("돌아돌아");
+            //isTurning = true;
+
+            for (int i = 0; i < TurnObject.Count; i++)
+            {
+                TurnObject[i].Turn();                         
+            }
+        }
+    }
+
+}
---
+++ Assets/Shader_Choi/Scripts/Turning/Turn_Info.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Turn_Info : MonoBehaviour
+{
+    [SerializeField] private int rotationdirection;
+
+    public void Turn()
+    {
+        Debug.Log("돌아간다");
+        //gameObject.transform.DOLocalRotate(new Vector3(gameObject.transform.rotation.x + rotationdirection * 90, gameObject.transform.rotation.y, gameObject.transform.rotation.z)
+        //, 2, RotateMode.LocalAxisAdd).SetEase(Ease.InQuad);
+        gameObject.transform.DOLocalRotateQuaternion(Quaternion.EulerAngles
+            (gameObject.transform.rotation.x + rotationdirection * 90, gameObject.transform.rotation.y, gameObject.transform.rotation.z), 2).SetEase(Ease.InQuad);
+
+
+    }
+}
