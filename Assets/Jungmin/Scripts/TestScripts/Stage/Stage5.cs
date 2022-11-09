b55f8d30f6eac7cca9b7135412f47dac1f4528fd
Jeongmin Kim <kjmgi0671@naver.com>
2022-11-09 18:29:22 +0900

    stage 3 fix

---
+++ Assets/Jungmin/Scripts/TestScripts/Stage/Stage5.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage5 : StageManager
+{
+    protected override void ClearCheck()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void StageClear()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
